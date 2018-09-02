aae20c277e61eda03c4a243e2d5a535c7ac1827c
Harry Bloch <harryfbloch@gmail.com>
2018-09-02 17:43:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, position)
-  puts " Position to be replaced#{board[position]}"
+  puts " Position to be replaced#{board}"
   if board[position] == "" || board[position] == " " || board[position] == nil
     return false
   elsif board[position] == "X" || board[position] == "O"
