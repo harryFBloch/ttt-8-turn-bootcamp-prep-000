9bc43f91ea1779cfb7a605646346d34cb8e08608
Harry Bloch <harryfbloch@gmail.com>
2018-09-02 17:40:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, position)
-  if position_taken?(board, position) != true && position < 9 && position > 0
+  if position_taken?(board, position) != true && position < 9 && position >= 0
     return true
   else
     return false
